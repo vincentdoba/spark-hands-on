6ebc06b4353f77a4a889a8c85b92e98aaab33787
Vincent Doba <vincent.doba@cedeela.fr>
2015-05-03 13:16:00 +0200

    clean data

---
+++ src/main/scala/psug/hands/on/exercise1/Winner.scala
@@ -0,0 +1,11 @@
+package psug.hands.on.exercise1
+
+/**
+ * Determine which is the winner of the 2007 French presidential election
+ *
+ * File : resultat_presidentielles_2012.csv
+ *
+ */
+object Winner extends App {
+
+}
