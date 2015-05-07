7980a7c7168736ef68862834fc71fa5f3888bdb4
Vincent Doba <vincent.doba@cedeela.fr>
2015-05-07 23:24:13 +0200

    Add exercise 1, simple transformation on integer RDDs

---
+++ src/main/scala/psug/hands/on/exercise01/SumOfSquareOfNonPrimeNumbers.scala
@@ -0,0 +1,21 @@
+package psug.hands.on.exercise01
+
+/**
+ * Compute the sum of squares of numbers that are under 100 and that are not prime.
+ *
+ * For instance, the same sum but for numbers that are under 10 is 1² + 4² + 6² + 8² + 9² + 10² = 308
+ *
+ * command : sbt "run-main psug.hands.on.exercise01.SumOfSquaresOfNonPrimeNumbers"
+ *
+ */
+object SumOfSquareOfNonPrimeNumbers extends App {
+
+  val startingNumbersList = 1 to 75
+  val endingNumbersList = 25 to 100
+  val primeNumbersList = List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+
+  val sumOfSquareOfNonPrimeNumbers = ??? // TODO compute the sum of squares of numbers under 100 that are not prime
+
+  println(s"The sum of square of numbers that are not prime and are under 100 is $sumOfSquareOfNonPrimeNumbers")
+
+}
